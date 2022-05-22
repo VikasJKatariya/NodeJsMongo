f785b44d597fa7e485efe26058e9ff939d4a9696
vikas katariya <vikas.katariya@mindinventory.com>
2022-05-22 09:33:05 +0530

    crud user

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const auth = require("./middleware/auth");
+const User = require("./model/user");
+const PORT = process.env.PORT  || 3000 
+const UserRoute = require('./routes/UserRoute')
+
 mongoose.connect('mongodb://localhost:27017/DEMO',{useNewUrlParser: true , useUnifiedTopology: true})
 require("dotenv").config();
 const db = mongoose.connection
@@ -14,16 +21,11 @@ const signUp = joi.object({
   email:joi.string().min(4).required().email(),
   password:joi.string().min(6).required()
 });
+
 const login = joi.object({
   email:joi.string().min(4).required().email(),
   password:joi.string().min(6).required()
 });
-var bcrypt = require('bcryptjs');
-var jwt = require("jsonwebtoken");
-const auth = require("./middleware/auth");
-
-
-const User = require("./model/user");
 
 db.on('error',(err) => {
     console.log(err)
@@ -39,47 +41,82 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.json());
 
-const PORT = process.env.PORT  || 3000 
-
 app.listen(PORT , () => {
     console.log(PORT)
 })
-
+app.use('/user/api',UserRoute)
 app.post("/register", async (req, res) => {
+  // Our register logic starts here
+  try {
 
-    // Our register logic starts here
-    try {
+    const { error } = signUp.validate(req.body);
+    // Error in response
+    res.send(error.details[0].message);
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
 
-      const { error } = signUp.validate(req.body);
-      // Error in response
-      res.send(error.details[0].message);
-      // Get user input
-      const { first_name, last_name, email, password } = req.body;
-  
-      // Validate user input
-      if (!(email && password && first_name && last_name)) {
-        res.status(400).send("All input is required");
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "30s",
       }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
   
-      // check if user already exist
-      // Validate if user exist in our database
-      const oldUser = await User.findOne({ email });
-  
-      if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-      }
-  
-      //Encrypt user password
-      encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create user in our database
-      const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-      });
-  
+app.post("/login", async (req, res) => {
+
+  // Our login logic starts here
+  try {
+
+    const { error } = login.validate(req.body);
+    // Error in response
+    res.send(error.details[0].message);
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
       // Create token
       const token = jwt.sign(
         { user_id: user._id, email },
@@ -88,58 +125,20 @@ app.post("/register", async (req, res) => {
           expiresIn: "30s",
         }
       );
+
       // save user token
       user.token = token;
-  
-      // return new user
-      res.status(201).json(user);
-    } catch (err) {
-      console.log(err);
+
+      // user
+      res.status(200).json(user);
     }
-    // Our register logic ends here
-  });
-  
-  app.post("/login", async (req, res) => {
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
 
-    // Our login logic starts here
-    try {
-
-      const { error } = login.validate(req.body);
-      // Error in response
-      res.send(error.details[0].message);
-      // Get user input
-      const { email, password } = req.body;
-  
-      // Validate user input
-      if (!(email && password)) {
-        res.status(400).send("All input is required");
-      }
-      // Validate if user exist in our database
-      const user = await User.findOne({ email });
-  
-      if (user && (await bcrypt.compare(password, user.password))) {
-        // Create token
-        const token = jwt.sign(
-          { user_id: user._id, email },
-          process.env.TOKEN_KEY,
-          {
-            expiresIn: "30s",
-          }
-        );
-  
-        // save user token
-        user.token = token;
-  
-        // user
-        res.status(200).json(user);
-      }
-      res.status(400).send("Invalid Credentials");
-    } catch (err) {
-      console.log(err);
-    }
-    // Our register logic ends here
-  });
-
-  app.post("/welcome", auth, (req, res) => {
-    res.status(200).send("Welcome 🙌 ");
-  });
+app.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
